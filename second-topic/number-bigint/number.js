48ce57412904c6e60188e9193910e1501d8d0965
Matheus <plutaozaum@gmail.com>
2023-07-15 02:11:40 -0300

    number and bigint, change target to ES2020

---
+++ second-topic/number-bigint/number.js
@@ -0,0 +1,9 @@
+"use strict";
+// ==> number abrange os tipos primitivos: int, float, positivo, negativo, infinito, NaN
+// ==> bigint: exemplo a seguir
+let num1 = 14.0;
+let num2 = 0x37cf;
+let num3 = 0o377;
+let num4 = 0b111001;
+let big1 = 301290391203910n;
+console.log(typeof big1); // ==> bigint
---
+++ second-topic/number-bigint/number.ts
@@ -0,0 +1,11 @@
+// ==> number abrange os tipos primitivos: int, float, positivo, negativo, infinito, NaN
+// ==> bigint: exemplo a seguir
+
+let num1: number = 14.0;
+let num2: number = 0x37cf;
+let num3: number = 0o377;
+let num4: number = 0b111001;
+
+let big1: bigint = 301290391203910n;
+
+console.log(typeof big1); // ==> bigint
