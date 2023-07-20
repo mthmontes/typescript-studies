441716483ec520c18e385edc2792a98622e48851
Matheus <plutaozaum@gmail.com>
2023-07-20 19:34:10 -0300

    just a string

---
+++ second-topic/strings/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let myname = "Matheus";
+let detailsName = `${myname} é o principal usuário desse perfil.`;
+console.log(detailsName);
---
+++ second-topic/strings/index.ts
@@ -0,0 +1,5 @@
+let myname:string = "Matheus";
+
+let detailsName: string = `${myname} é o principal usuário desse perfil.`
+
+console.log(detailsName);
